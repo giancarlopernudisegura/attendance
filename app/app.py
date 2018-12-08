d480e7adb725ff08dfc9e72f50f4fcd722c75b31
giancarlopernudisegura <giancarlopernudi@hotmail.com>
2018-12-08 01:30:09 -0700

    Succesful creation

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import CreateAttendance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'FEDD82CFAC8127DDDF9A483EB95E7'
@@ -8,8 +8,11 @@ app.config['SECRET_KEY'] = 'FEDD82CFAC8127DDDF9A483EB95E7'
 @app.route("/create", methods=['GET', 'POST'])
 def create():
     form = CreateAttendance()
-    # if (f.validate()):
-    #     return render_template('attendance.html', f=f)
+    if (form.validate_on_submit()):
+        flash('sign in sheet created for {}'.format(
+            form.entrydate.data), 'success')
+        return redirect(url_for('attendance'))
+        # return render_template('attendance.html', f=f)
     return render_template('create.html', f=form)
 
 
